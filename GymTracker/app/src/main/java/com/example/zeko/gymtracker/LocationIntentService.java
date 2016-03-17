725406f784de77d35e7a6d67d3b3942c76265114
Steaton <steaton@gmail.com>
2016-03-17 21:40:41 +0000

    Fixed typo and started moving location stuff into the service.

--- GymTracker/app/src/main/java/com/example/zeko/gymtracker/LocationIntentService.java
+++ GymTracker/app/src/main/java/com/example/zeko/gymtracker/LocationIntentService.java
@@ -2,8 +2,14 @@ package com.example.zeko.gymtracker;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.location.Location;
+import android.os.Bundle;
 
-public class LocationIntentService extends IntentService {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+
+public class LocationIntentService extends IntentService implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     public LocationIntentService() {
         super("LocationIntentService");
@@ -11,6 +17,26 @@ public class LocationIntentService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        System.out.println("This is the locatio:  XXX");
+        System.out.println("This is the location:  XXX");
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
     }
 }
