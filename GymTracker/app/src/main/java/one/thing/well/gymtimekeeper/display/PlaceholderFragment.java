579b92f971188970ad80c2a9e2a79c3d129cde5f
ZakaZeko <zakrea.123456789@yahoo.co.uk>
2016-03-22 22:16:49 +0000

    Update PlaceholderFragment.java
    Just one line of code for empty catch block

--- GymTracker/app/src/main/java/one/thing/well/gymtimekeeper/display/PlaceholderFragment.java
+++ GymTracker/app/src/main/java/one/thing/well/gymtimekeeper/display/PlaceholderFragment.java
@@ -111,6 +111,8 @@ public class PlaceholderFragment extends android.support.v4.app.Fragment {
                         });
                     }
                 } catch (InterruptedException e) {
+                    e.printStackTrace();
+
                 }
             }
         };
