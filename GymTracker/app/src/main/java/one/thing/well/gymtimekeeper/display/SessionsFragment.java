0388d069f12093e9f1eb9b5ab4bbfa40a5421c50
Steaton <steaton@gmail.com>
2016-04-22 19:27:53 +0100

    Some small formatting changes to code in SessionsFragment

--- GymTracker/app/src/main/java/one/thing/well/gymtimekeeper/display/SessionsFragment.java
+++ GymTracker/app/src/main/java/one/thing/well/gymtimekeeper/display/SessionsFragment.java
@@ -7,30 +7,25 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.R.layout;
 
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import one.thing.well.gymtimekeeper.GymTimekeeperApplication;
 import one.thing.well.gymtimekeeper.R;
-import one.thing.well.gymtimekeeper.datastore.locationevent.LocationEventFile;
 import one.thing.well.gymtimekeeper.datastore.sessionsummary.SessionSummaryEntry;
 import one.thing.well.gymtimekeeper.datastore.sessionsummary.SessionSummaryFile;
 import one.thing.well.gymtimekeeper.session.SessionSummary;
-import one.thing.well.gymtimekeeper.util.DateUtils;
 
-/**
- * Created by zeko on 05/04/2016.
- */
-public class SessionsFragment extends android.support.v4.app.Fragment{
+public class SessionsFragment extends android.support.v4.app.Fragment {
 
-    ListView SessionsListVIew;
-    ArrayList<String> mylist = new ArrayList<>();
-    private ArrayAdapter<String> ArraAdapterForSessions;
+    private ListView sessionsListView;
+
+    private ArrayList<String> mylist = new ArrayList<>();
+
+    private ArrayAdapter<String> arrayAdapterForSessions;
 
     public static SessionsFragment newInstance() throws IOException, ParseException {
         SessionsFragment fragment = new SessionsFragment();
@@ -45,13 +40,12 @@ public class SessionsFragment extends android.support.v4.app.Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View rootView = inflater.inflate(R.layout.sessions_fragment,container,false);
         TextView SessionsTextView = (TextView) rootView.findViewById(R.id.SessionsTextView);
-        SessionsListVIew = (ListView) rootView.findViewById(R.id.SessionsListView);
-        ArraAdapterForSessions = new ArrayAdapter<String>(GymTimekeeperApplication.getAppContext(),R.layout.css_for_the_sessions_fragment_list_view, mylist);
-        SessionsListVIew.setAdapter(ArraAdapterForSessions);
-        ArraAdapterForSessions.notifyDataSetChanged();
+        sessionsListView = (ListView) rootView.findViewById(R.id.SessionsListView);
+        arrayAdapterForSessions = new ArrayAdapter<String>(GymTimekeeperApplication.getAppContext(),R.layout.css_for_the_sessions_fragment_list_view, mylist);
+        sessionsListView.setAdapter(arrayAdapterForSessions);
+        arrayAdapterForSessions.notifyDataSetChanged();
         return rootView;
     }
-
 
     private void GatheringDataForSessionsFragment() throws IOException, ParseException {
         SessionSummary sessionSummary =  new SessionSummary();
